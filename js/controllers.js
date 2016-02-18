d2dcc79cf437364c00a0775fe003d80debffc9a8
Muhammad Yafi <yafithekid212@gmail.com>
2016-02-18 10:54:05 +0700

    add controllers and services (base, not finished)

--- js/controllers.js
+++ js/controllers.js
@@ -2,16 +2,46 @@ var appCtrls = angular.module('etollControllers',[
     'etollServices'
 ]);
 
-appCtrls.controller('loginCtrl',['$scope',function($scope){
+appCtrls.controller('loginCtrl',['$scope','etollApi',function($scope,etollApi){
+
+    $scope.submit = function(email,password){
+        etollApi.login(email,password)
+            .then(function(data){
+                if(data.status == 'ok'){
+                    //TODO save to cookies maybe
+                } else {
+                    //TODO handle error
+                }
+            })
+            .catch(function(message){
+                //TODO impl
+                alert(message);
+            });
+    };
 
 }]);
 
-appCtrls.controller('tollCtrl',['$scope',function($scope){
+appCtrls.controller('tollCtrl',['$scope','etollApi','mockData',function($scope,etollApi,mockData){
+    //TODO change to not mock
+    $scope.sourceTolls = mockData.fromToll;
+    $scope.vehicleCategories = mockData.vehicleCategories;
 
+    $scope.submit = function(plateNo,sourceTollId,destTollId,price){
+        etollApi.createActivity(plateNo,sourceTollId,destTollId,price)
+            .then(function(data){
+                if (data.status == 'ok'){
+                    //TODO impl
+                } else {
+                    //TODO impl
+                }
+            })
+            .catch(function(message){
+                alert(message);
+            })
+    };
 }]);
 
 appCtrls.controller('listStaffCtrl',['$scope',function($scope){
-
 }]);
 
 appCtrls.controller('addStaffCtrl',['$scope',function($scope){
--- js/services.js
+++ js/services.js
@@ -1,3 +1,71 @@
-var appServices = angular.module('etollServices',[
+var appServices = angular.module('etollServices',[]);
+appServices.service('etollApiUrl',function(){
+    var BASE_URL = "http://localhost:8080/etollapi";
 
-]);
+    this.login = function(email,password){
+        return BASE_URL + "/login?email="+email+"&password="+password;
+    };
+
+    this.createActivity = function(plate_no,source_toll_id,dest_toll_id,price){
+        return BASE_URL + "/activities/create?plate_no="+plate_no
+        +"&source_toll_id="+source_toll_id+"&dest_toll_id="+dest_toll_id+"&price="+price;
+    };
+
+    this.getTolls = function(){
+        return BASE_URL + "/tolls/";
+    };
+
+});
+appServices.service('etollApi',['$http','$q','etollApiUrl',function($http,$q,etollApiUrl){
+    this.login = function(email,password){
+        var url = etollApiUrl.login(email,password);
+        var deferred = $q.defer();
+        $http.get(url)
+            .success(function(data){
+                deferred.resolve(data);
+            })
+            .error(function(data,status){
+                deferred.reject(status+" "+data);
+            });
+        return deferred.promise;
+    };
+
+    this.createActivity = function(msisdn,token,plate_no,source_toll_id,dest_toll_id,price){
+        var url = etollApiUrl.createActivity(msisdn,token,plate_no,source_toll_id,dest_toll_id,price);
+        var deferred = $q.defer();
+        $http.get(url)
+            .success(function(data){
+                deferred.resolve(data);
+            })
+            .error(function(data,status){
+                deferred.reject(status+ " " + data);
+            });
+        return deferred.promise;
+    };
+
+    this.getTolls = function(){
+        var url = etollApiUrl.getTolls();
+        var deferred = $q.defer();
+        $http.get(url)
+            .success(function(data){
+                deferred.resolve(data);
+            })
+            .error(function(data,status){
+                deferred.reject(status+" " +data);
+            });
+        return deferred.promise;
+    }
+}]);
+
+appServices.factory('mockData',function(){
+    return {
+        fromToll : [
+            { id: 1, name: "toll 1" },
+            { id: 2, name: "toll 2" }
+        ],
+        vehicleCategories : [
+            { id : 1, name: "Mobil" },
+            { id : 2, name: "Becak" }
+        ]
+    };
+});
